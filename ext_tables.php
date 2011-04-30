34126777155dcc7da25a95dcae0202c15dc9e0d9
adgrafik <adgrafik@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-04-30 15:26:47 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ad_google_maps_api/trunk@47156 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (!defined ('TYPO3_MODE')) die ('Access denied.');
-
-// Add static TypoScript
-t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript/', 'ad: Google Maps API');
-
-?>
